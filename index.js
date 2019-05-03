d328713c832998f04357f514247d85e579e25cbd
ttakkku <k2005118@gmail.com>
2019-05-03 19:27:56 +0900

    bug fix

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function plus(a, b){
+exports.plus = function (a, b) {
    return a + b
 }
-function Minus(a, b){
+exports.Minus = function (a, b) {
     return a - b
 }
-function multiply(a, b) {
+exports.multiply = function (a, b) {
     return a * b
 }
-function division(a, b){
+exports.division = function (a, b) {
     return a / b
 }
-function caret(a, b){
+exports.caret = function (a, b) {
     return a ^ b
 }
