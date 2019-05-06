d5f0c7f2085494b18c2939703be55bafb3e83df4
ttakkku <ttakkku@outlook.com>
2019-05-06 15:24:26 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const SIM = require('./index.js');
 
-console.log(SIM.pow(10, 100)); //working fine.
-console.log(SIM.pi2(1234));
-console.log(SIM.pi);
+Console.log(SIM.pow(10, 100)); //working fine.
+Console.log(SIM.pi2(1234));
+Console.log(SIM.pi);
